909a5240bac0438015c776adc35a23ca0ba4d1db
pml <42704376+pmlogist@users.noreply.github.com>
2024-01-09 23:34:04 +0100

    Simplify zsh in options

--- modules/shells/zsh.nix
+++ modules/shells/zsh.nix
@@ -2,71 +2,14 @@
 with lib;
 let
   cfg = config.apps.zsh;
-
-  # pulled from GitHub
-  # https://github.com/nix-community/home-manager/blob/master/modules/programs/zsh.nix
-  pluginModule = types.submodule ({ config, ... }: {
-    options = {
-      src = mkOption {
-        type = types.path;
-        description = ''
-          Path to the plugin folder.
-
-          Will be added to {env}`fpath` and {env}`PATH`.
-        '';
-      };
-
-      name = mkOption {
-        type = types.str;
-        description = ''
-          The name of the plugin.
-
-          Don't forget to add {option}`file`
-          if the script name does not follow convention.
-        '';
-      };
-
-      file = mkOption {
-        type = types.str;
-        description = "The plugin script to source.";
-      };
-    };
-
-    config.file = mkDefault "${config.name}.plugin.zsh";
-  });
 in
 {
   options.apps.zsh = {
     enable = mkEnableOption "zsh shell";
 
     plugins = mkOption {
-      type = types.listOf pluginModule;
+      type = with types; listOf attrs;
       default = [ ];
-      example = literalExpression ''
-        [
-          {
-            # will source zsh-autosuggestions.plugin.zsh
-            name = "zsh-autosuggestions";
-            src = pkgs.fetchFromGitHub {
-              owner = "zsh-users";
-              repo = "zsh-autosuggestions";
-              rev = "v0.4.0";
-              sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-            };
-          }
-          {
-            name = "enhancd";
-            file = "init.sh";
-            src = pkgs.fetchFromGitHub {
-              owner = "b4b4r07";
-              repo = "enhancd";
-              rev = "v2.2.1";
-              sha256 = "0iqa9j09fwm6nj5rpip87x3hnvbbz9w9ajgm6wkrd5fls8fn8i5g";
-            };
-          }
-        ]
-      '';
-      description = "Plugins to source in {file}`.zshrc`.";
     };
   };
 
@@ -118,7 +61,7 @@ in
             bindkey '^[OB' history-substring-search-down
             bindkey '^[[B' history-substring-search-down
           '';
-          plugins = [
+          plugins = cfg.plugins ++ [
             {
               name = "typewritten";
               src = pkgs.fetchFromGitHub {
@@ -138,8 +81,7 @@ in
                 sha256 = "RVX9ZSzjBW3LpFs2W86lKI6vtcvDWP6EPxzeTcRZua4=";
               };
             }
-          ]
-          ++ cfg.plugins;
+          ];
         };
       };
     }
