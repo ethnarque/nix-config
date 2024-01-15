41253c51b59aaf7e2c700e8ef86d6d89f7035139
pml <42704376+pmlogist@users.noreply.github.com>
2024-01-15 18:05:54 +0100

    Create password-store module

---
+++ modules/applications/security/pass.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  system,
+  username,
+  ...
+}:
+with lib; let
+  cfg = config.apps.pass;
+in {
+  options.apps.pass = {
+    enable = mkEnableOption "password-store";
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    (
+      if !(builtins.elem system ["aarch64-darwin" "x86_64-darwin"])
+      then {
+        home-manager.users.${username} = {
+          programs.password-store.enable = true;
+        };
+      }
+      else {}
+    )
+  ]);
+}
