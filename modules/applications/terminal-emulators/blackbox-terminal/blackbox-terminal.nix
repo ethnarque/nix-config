fa2af42c86d8cf8d493ef7844855bbfb526992f8
pml <42704376+pmlogist@users.noreply.github.com>
2024-01-17 02:18:38 +0100

    Fix unicode character

--- modules/applications/terminal-emulators/blackbox-terminal/blackbox-terminal.nix
+++ modules/applications/terminal-emulators/blackbox-terminal/blackbox-terminal.nix
@@ -17,31 +17,30 @@ in {
       pkgs.blackbox-terminal
     ];
 
-    home-manager.users.${username} = let
+    home-manager.users.${username} = with lib.hm.gvariant; let
       fonts = config.compositors.interface.fonts;
     in
-      {config, ...}:
-        with lib.hm.gvariant; {
-          dconf.settings."com/raggesilver/BlackBox" = {
-            context-aware-header-bar = true;
-            fill-tabs = false;
-            floating-controls = false;
-            font = "${fonts.monospace.name} ${toString fonts.monospace.size}";
-            headerbar-drag-area = false;
-            show-headerbar = true;
-            show-menu-button = true;
-            style-preference = mkUint32 0;
-            terminal-bell = false;
-            terminal-padding = mkTuple [(mkUint32 5) (mkUint32 5) (mkUint32 5) (mkUint32 5)];
-            theme-dark = "Ros\233 Pine";
-            theme-light = "Ros\233 Pine Dawn";
-            was-maximized = false;
-          };
-
-          home.file."${config.xdg.dataHome}/blackbox/schemes" = {
-            source = ./themes;
-            recursive = true;
-          };
+      {config, ...}: {
+        dconf.settings."com/raggesilver/BlackBox" = {
+          context-aware-header-bar = true;
+          fill-tabs = false;
+          floating-controls = false;
+          font = "${fonts.monospace.name} ${toString fonts.monospace.size}";
+          headerbar-drag-area = false;
+          show-headerbar = true;
+          show-menu-button = true;
+          style-preference = mkUint32 0;
+          terminal-bell = false;
+          terminal-padding = mkTuple [(mkUint32 5) (mkUint32 5) (mkUint32 5) (mkUint32 5)];
+          theme-dark = "Rosé Pine";
+          theme-light = "Rosé Pine Dawn";
+          was-maximized = false;
         };
+
+        home.file."${config.xdg.dataHome}/blackbox/schemes" = {
+          source = ./themes;
+          recursive = true;
+        };
+      };
   };
 }
