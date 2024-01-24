c66dd33af2bae80471b8ed6f6a50eb277a7997bc
pml <42704376+pmlogist@users.noreply.github.com>
2024-01-24 19:33:31 +0100

    Add first implementation of darkman

--- modules/applications/terminal-emulators/kitty/kitty.nix
+++ modules/applications/terminal-emulators/kitty/kitty.nix
@@ -27,11 +27,14 @@ in {
     home-manager.users.${username} = {
       programs.kitty = {
         enable = true;
+
         theme = "Rosé Pine";
+
         font = {
           name = cfg.font.name;
           size = cfg.font.size;
         };
+
         settings = {
           enable_audio_bell = false;
           allow_remote_control = true;
@@ -40,11 +43,46 @@ in {
           hide_window_decorations =
             if pkgs.stdenv.isDarwin
             then "titlebar-only"
-            else true;
+            else false;
           macos_quit_when_last_window_closed = false;
           macos_option_as_alt = false;
+          wayland_titlebar_color = "system";
         };
+
+        extraConfig = ''
+          include current_theme.conf
+        '';
+
         shellIntegration.enableZshIntegration = true;
+      };
+
+      services.darkman = {
+        enable = true;
+
+        settings = {
+          lat = 45.7;
+          lng = 4.9;
+          usegeoclue = true;
+        };
+
+        darkModeScripts = {
+          gtk-theme = ''
+            ${pkgs.dconf}/bin/dconf write \
+                /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+          '';
+          kitty = ''
+            ${pkgs.kitty}/bin/kitty +kitten themes --reload-in=all --config-file-name "current_theme.conf" Rosé\ Pine
+          '';
+        };
+        lightModeScripts = {
+          gtk-theme = ''
+            ${pkgs.dconf}/bin/dconf write \
+                /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+          '';
+          kitty = ''
+            ${pkgs.kitty}/bin/kitty +kitten themes --reload-in=all --config-file-name "current_theme.conf" Rosé\ Pine\ Dawn
+          '';
+        };
       };
     };
   };
