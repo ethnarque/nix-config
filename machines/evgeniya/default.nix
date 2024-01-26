57057ab7f175ff13363282ff81f7605320b25694
pml <42704376+pmlogist@users.noreply.github.com>
2024-01-26 18:11:19 +0100

    Create  package + cleanups

--- machines/evgeniya/default.nix
+++ machines/evgeniya/default.nix
@@ -19,100 +19,98 @@ in {
     foliate
     gimp
     jq
-    obinskit
     telegram-desktop
     yt-dlp
   ];
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-13.6.9"
-  ];
-
-  machines.linux.enable = true;
-  machines.linux.hostName = "evgeniya";
-  machines.linux.hardware.intel.enable = true;
+  machines.linux = {
+    enable = true;
+    hostName = "evgeniya";
+    hardware.intel.enable = true;
+  };
 
   compositors.gnome.enable = true;
 
-  compositors.interface.fonts = {
-    packages = with pkgs; [
-      apple-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      iosevka
-      (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
-    ];
-    sans = {
-      name = "SF Pro";
-      size = 11.0;
-    };
-    serif = {
-      name = "New York Medium";
-      size = 11.0;
-    };
-    monospace = {
-      name = "Iosevka";
-      size = 11.0;
-    };
-  };
-
-  compositors.interface.icons = {
-    name = "Papirus-Dark";
-    package = pkgs.papirus-icon-theme;
-  };
-
-  compositors.interface.cursor = {
-    name = "Adwaita";
-    package = pkgs.gnome.adwaita-icon-theme;
-  };
-
-  apps.blackbox-terminal.enable = true;
-
-  apps.btop.enable = true;
-
-  apps.firefox = {
-    enable = true;
-    bookmarks = [
-      {
-        name = "GitHub - ethnarque";
-        tags = ["ethnarque" "github"];
-        keyword = "github";
-        url = "https://github.com/ethnarque";
-      }
-    ];
-    settings = {
-      "mousewheel.default.delta_multiplier_y" = 25; # No scroll factor options in gnome
-    };
-  };
-
-  apps.git = {
-    enable = true;
-    extraConfig = {
-      user = {
-        email = "42704376+pmlogist@users.noreply.github.com";
-        name = "pml";
+  compositors.interface = {
+    fonts = {
+      packages = with pkgs; [
+        apple-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        iosevka
+        (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
+      ];
+      sans = {
+        name = "SF Pro";
+        size = 11.0;
+      };
+      serif = {
+        name = "New York Medium";
+        size = 11.0;
+      };
+      monospace = {
+        name = "Iosevka";
+        size = 11.0;
       };
     };
-  };
 
-  apps.gh.enable = true;
+    icons = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
 
-  apps.kitty.enable = true;
-
-  apps.mpv = {
-    enable = true;
-    config = {
-      gpu-context = "wayland";
+    cursor = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
     };
   };
 
-  apps.pass.enable = true;
+  apps = {
+    btop.enable = true;
 
-  apps.tmux.enable = true;
+    firefox = {
+      enable = true;
+      bookmarks = [
+        {
+          name = "GitHub - ethnarque";
+          tags = ["ethnarque" "github"];
+          keyword = "github";
+          url = "https://github.com/ethnarque";
+        }
+      ];
+      settings = {
+        "mousewheel.default.delta_multiplier_y" = 25; # No scroll factor options in gnome
+      };
+    };
 
-  apps.zsh = {
-    enable = true;
-    plugins = [];
+    git = {
+      enable = true;
+      extraConfig = {
+        user = {
+          email = "42704376+pmlogist@users.noreply.github.com";
+          name = "pml";
+        };
+      };
+    };
+
+    gh.enable = true;
+
+    kitty.enable = true;
+
+    mpv = {
+      enable = true;
+      config = {
+        gpu-context = "wayland";
+      };
+    };
+
+    obinskit.enable = true;
+
+    pass.enable = true;
+
+    tmux.enable = true;
+
+    zsh.enable = true;
   };
 
   programs.gnupg.agent = {
@@ -120,7 +118,8 @@ in {
     enableSSHSupport = true;
   };
 
-  services'.samba.enable = true;
-
-  services'.ssh.enable = true;
+  services' = {
+    samba.enable = true;
+    ssh.enable = true;
+  };
 }
