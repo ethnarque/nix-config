1317d259133769f11266840378fe60e7c9b051bd
pml <42704376+pmlogist@users.noreply.github.com>
2024-03-14 22:00:25 +0100

    Format input

--- flake.nix
+++ flake.nix
@@ -7,8 +7,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
-    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager.url = "github:nix-community/home-manager";
 
